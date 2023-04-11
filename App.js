d6a43dbf18f50b86070bdc71782bb04c86f65e35
chrisdionisius <chrisdionisius@gmail.com>
2023-04-11 16:40:09 +0700

    add product detail page

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/screens/ProductScreen";
+import ProductDetailScreen from "./src/screens/ProductDetailScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductScreen />
+      {/* <ProductScreen /> */}
+      <ProductDetailScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
